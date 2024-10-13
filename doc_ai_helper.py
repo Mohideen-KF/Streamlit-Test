0cfe07c2760fd123e5b5dd32cacefa303e52562a
Mohideen-KF <mohideen@kissflow.com>
2024-10-13 21:56:20 +0530

    Update doc_ai_helper.py

--- doc_ai_helper.py
+++ doc_ai_helper.py
@@ -61,12 +61,12 @@ if st.session_state.qa_model is None or st.session_state.embedding_model is None
 
 if st.session_state.docs is None:
     with st.spinner("Fetching documentation..."):
-        st.session_state.docs = fetch_docs("https://community.kissflow.com/category/documentation-section/")
+        st.session_state.docs = fetch_docs("https://docs.python.org/3/library/")
         processed_docs = [preprocess(doc) for doc in st.session_state.docs.values()]
         st.session_state.embeddings = st.session_state.embedding_model.encode(processed_docs)
 
 # Get user input
-user_input = st.text_input("Ask a question about Kissflow:")
+user_input = st.text_input("Ask a question about Python:")
 
 if user_input:
     with st.spinner("Generating response..."):
